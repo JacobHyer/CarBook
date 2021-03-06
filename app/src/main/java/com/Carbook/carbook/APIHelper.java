bbcadeec5a6aa4bf5bc3a0abd3d02c4aa7a14b44
Tyler Call <tylercall1996@gmail.com>
2021-03-06 12:06:28 -0600

    Initial API creation +NHTSA

---
+++ app/src/main/java/com/Carbook/carbook/APIHelper.java
@@ -0,0 +1,48 @@
+package com.Carbook.carbook;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
+
+public class APIHelper {
+
+    final String vinBaseURL = "https://vpic.nhtsa.dot.gov/api/vehicles/decodevinvalues/";
+    private String url = "";
+
+    public String makeCall(String method, List<String> params) {
+        switch (method) {
+            case "vin":
+                url += vinBaseURL;
+                for (int i = 0; i < params.size(); i++) {
+                    url += params.get(i);
+                }
+                url += "?format=json";
+                break;
+            case "make/mocel":
+                break;
+        }
+        try {
+            URL urlObj = new URL(url);
+            HttpURLConnection connection = (HttpURLConnection) urlObj.openConnection();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            StringBuilder data = new StringBuilder();
+            String line;
+
+            do {
+                line = reader.readLine();
+                if (line != null) {
+                    data.append(line);
+                }
+            }
+            while (line != null);
+
+            return data.toString();
+        } catch (IOException ioe) {
+            System.out.println("Error reading HTTP Response: " + ioe);
+            return null;
+        }
+    }
+}
