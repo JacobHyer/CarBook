8a72b20cc1b21aae859f0ce62fed6f4a12f95adf
rdavis0 <rdfiasco@gmail.com>
2021-03-27 20:05:45 -0600

    Added view car activity and start of RecyclerView for list of maintenance items

---
+++ app/src/main/java/com/Carbook/carbook/MaintenanceAdapter.java
@@ -0,0 +1,12 @@
+package com.Carbook.carbook;
+
+import android.view.View;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MaintenanceAdapter {
+
+    private class MaintenanceViewHolder(View itemView)  {
+
+    }
+}
---
+++ app/src/main/java/com/Carbook/carbook/ViewCarActivity.java
@@ -0,0 +1,37 @@
+package com.Carbook.carbook;
+
+import android.os.Bundle;
+
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+public class ViewCarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // Car object for testing
+        Car car = new Car(null, "Nissan", "Maxima", "2012", 70000);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_car);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        CollapsingToolbarLayout toolBarLayout = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
+        toolBarLayout.setTitle(getTitle());
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+}
