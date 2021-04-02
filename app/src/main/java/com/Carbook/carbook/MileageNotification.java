83ccd83fdeca944d4e4ff2e5472a66224818009e
Tyler Call <tylercall1996@gmail.com>
2021-04-02 15:53:52 -0500

    Update for adjusted Mileage Activity

--- app/src/main/java/com/Carbook/carbook/MileageNotification.java
+++ app/src/main/java/com/Carbook/carbook/MileageNotification.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Parcelable;
 import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.TextView;
@@ -16,6 +15,8 @@ public class MileageNotification extends AppCompatActivity {
 
     private TextView carName;
     Car car;
+
+    public static final String TAG = "MileageNotification";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,9 +41,15 @@ public class MileageNotification extends AppCompatActivity {
     }
 
     public void selectYes(View view) {
+        long diffDays = (car.getMileageChanged().getTimeInMillis())
+                - (Calendar.getInstance().getTimeInMillis())
+                / (24 * 60 *60 * 1000); //converts milliseconds to days
+        int estimatedMiles = (int)((car.getAvgMiles() / 7) * diffDays); //Converts car's avgMiles to avgMiles per day, then multiplies by diffDays
         Intent intent = new Intent(this, MileageActivity.class);
-        intent.putExtra("REQUESTCODE", 2);
-        intent.putExtra("ID", car.getId());
+        intent.putExtra("uniqueId", TAG);
+        intent.putExtra("carId", car.getId());
+        intent.putExtra(MileageActivity.EXTRA_MILEAGE, estimatedMiles);
+        intent.putExtra(MileageActivity.EXTRA_AVG_MILEAGE, car.getAvgMiles());
         startActivityForResult(intent, 2);
     }
     public void selectNo(View view) {
@@ -56,7 +63,7 @@ public class MileageNotification extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
         super.onActivityResult(requestCode, resultCode, intent);
-        if (resultCode == 2) {
+        if (resultCode == 1) {
             Intent dash = new Intent(this, DashboardActivity.class);
             Toast.makeText(this, "Mileage updated", Toast.LENGTH_SHORT).show();
             startActivity(dash);
