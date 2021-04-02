c625224afe7b35e45730af891d575e48c304f3e9
Tyler Call <tylercall1996@gmail.com>
2021-04-02 15:47:19 -0500

    Consolidate saving mileage into one activity

--- app/src/main/java/com/Carbook/carbook/MileageActivity.java
+++ app/src/main/java/com/Carbook/carbook/MileageActivity.java
@@ -18,8 +18,11 @@ public class MileageActivity extends AppCompatActivity {
     private DBHelper db;
     private int miles;
     private int milesAvg;
-    private int code;
-    private long id;
+    private String calledBy;
+    private long carId;
+
+    public static final String EXTRA_MILEAGE = "MILEAGE";
+    public static final String EXTRA_AVG_MILEAGE = "AVG_MILEAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,21 +31,21 @@ public class MileageActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         //Used to identify if the activity was started from a notification or from an activity
-        code = intent.getIntExtra("REQUESTCODE", -1);
-        id = intent.getLongExtra("ID", -1);
+        calledBy = intent.getStringExtra("uniqueId");
+        carId = intent.getLongExtra("carId", -1);
+        miles = intent.getIntExtra(EXTRA_MILEAGE, 0);
+        milesAvg = intent.getIntExtra(EXTRA_AVG_MILEAGE, 0);
+
+
         db = new DBHelper(this);
 
         userMileage = findViewById(R.id.user_mileage);
         userAvg = findViewById(R.id.user_avg);
 
-        // Set existing mileage values if called from ViewCarActivity
-        String calledBy = getIntent().getStringExtra("uniqueId");
-        if(calledBy != null && calledBy.equals(ViewCarActivity.TAG)) {
-            miles = getIntent().getIntExtra(ViewCarActivity.EXTRA_MILEAGE, 0);
-            milesAvg = getIntent().getIntExtra(ViewCarActivity.EXTRA_AVG_MILEAGE, 0);
+        // Set existing mileage values if called from ViewCarActivity or MileageNotification
+        if(calledBy != null && (calledBy.equals(ViewCarActivity.TAG) || calledBy.equals(MileageNotification.TAG))) {
             userMileage.setText(String.valueOf(miles));
             userAvg.setText(String.valueOf(milesAvg));
-
         }
 
         userMileage.addTextChangedListener(new TextWatcher() {
@@ -91,20 +94,28 @@ public class MileageActivity extends AppCompatActivity {
     public void saveMileage(View view) {
         //TODO: We may want to consolidate all code into this format to have this activity update the databases, then create the intent specific to which activity needs to be opened (based on request codes?)
         //Request code 2 signifies that saving will need to return the user to the DashboardActivity
-        if (code == 2) {
-            Boolean success = db.updateField("cars", id, "mileage", String.valueOf(miles));
-            if(success) success = db.updateField("cars", id, "avg_miles", String.valueOf(milesAvg));
-            if (success) {
-                Intent intent = new Intent(this, MileageNotification.class);
-                setResult(2, intent);
-                finish();
-            }
-        } else {
-            Intent intent = new Intent(this,AddCarActivity.class);
-            intent.putExtra("MILEAGE", miles);
-            intent.putExtra("AVG_MILEAGE", milesAvg);
+        Intent intent = null;
+        if (calledBy != null && calledBy == MileageNotification.TAG) {
+            intent = new Intent(this, MileageNotification.class);
+        } else if (calledBy != null && calledBy == ViewCarActivity.TAG) {
+            intent = new Intent(this, ViewCarActivity.class);
+        } else if (calledBy != null && calledBy == AddCarActivity.TAG) {
+            //Will finish and send back to AddCarActivity since there is no entry in the database for the new car
+            intent = new Intent(this, AddCarActivity.class);
+            intent.putExtra(EXTRA_MILEAGE, miles);
+            intent.putExtra(EXTRA_AVG_MILEAGE, milesAvg);
             setResult(1, intent);
             finish();
+        }
+        Boolean success = db.updateField("cars", carId, "mileage", String.valueOf(miles));
+        if(success) success = db.updateField("cars", carId, "avg_miles", String.valueOf(milesAvg));
+        if (success) {
+            //Result code 1 signifies that the update was successful
+            setResult(1, intent);
+            finish();
+        } else {
+            //Result code 0 signifies that the update failed
+            setResult(0, intent);
         }
     }
 
