8837578458f18e0ac22eab80d1c6c9fc8b62de42
Tyler Call <tylercall1996@gmail.com>
2021-04-02 20:49:46 -0500

    Add Date Picker

--- app/src/main/java/com/Carbook/carbook/UpdateMaintenanceActivity.java
+++ app/src/main/java/com/Carbook/carbook/UpdateMaintenanceActivity.java
@@ -1,14 +1,23 @@
 package com.Carbook.carbook;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
+
+import java.util.Calendar;
 
 public class UpdateMaintenanceActivity extends AppCompatActivity {
 
+    private DatePickerDialog picker;
+    private EditText dateText;
     private EditText maintDesc;
     private EditText maintMileage;
     private EditText maintNote;
@@ -27,11 +36,32 @@ public class UpdateMaintenanceActivity extends AppCompatActivity {
         db = new DBHelper(this);
 
         car = (Car)getIntent().getSerializableExtra("CAR");
+
+        dateText = (EditText) findViewById(R.id.editTextDate);
+        dateText.setInputType(InputType.TYPE_NULL);
+        dateText.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onClick (View view) {
+                final Calendar cal = Calendar.getInstance();
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+                int month = cal.get(Calendar.MONTH);
+                int year = cal.get(Calendar.YEAR);
+                picker = new DatePickerDialog(UpdateMaintenanceActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                                dateText.setText((month + 1) + "/" + dayOfMonth + "/" + year);
+                            }
+                        }, year, month, day);
+                picker.show();
+            }
+        });
     }
 
     public void saveItem(View view) {
         //TODO: Save maintenance item to the database/car object
-        item.setDescription(maintDesc.getText().toString());
+        /*item.setDescription(maintDesc.getText().toString());
         item.setNotes(maintNote.getText().toString());
         item.setMileage(Integer.parseInt(maintMileage.getText().toString()));
         //TODO: Calendars for maint and see if IDs can be put as same type
@@ -46,7 +76,7 @@ public class UpdateMaintenanceActivity extends AppCompatActivity {
             Intent intent = new Intent(this, ViewCarActivity.class);
             setResult(2, intent);
             finish();
-        }
+        }*/
     }
 
     public void cancelItem(View view) {
